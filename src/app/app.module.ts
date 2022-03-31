1ff39544afdeea8671d419b4d78a83ce061b7e44
Shyam7675 <shyamsoni954@gmail.com>
2022-03-31 23:55:50 +0530

    1 file modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { SecondComponent } from './second/second.component';
+import { FirstComponent } from './first/first.component';
 
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent],
+  declarations: [AppComponent, HelloComponent,SecondComponent,FirstComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
